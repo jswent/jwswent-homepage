3966c129175c1617b23eab8ffe93bf306e70c273
jswent <94636565+jswent@users.noreply.github.com>
2022-07-30 23:54:45 -0400

    added source to navbar

--- components/navbar.js
+++ components/navbar.js
@@ -16,16 +16,19 @@ import {
 } from '@chakra-ui/react'
 import { HamburgerIcon } from '@chakra-ui/icons'
 import ThemeToggleButton from './theme-toggle-button'
+import { IoLogoGithub } from 'react-icons/io5'
 
-const LinkItem = ({ href, path, children }) => {
+const LinkItem = ({ href, path, target, children, ...props }) => {
   const active = path === href
   const inactiveColor = useColorModeValue('gray200', 'whiteAlpha.900')
   return (
-    <NextLink href={href}>
+    <NextLink href={href} passHref scroll={false}>
       <Link
         p={2}
         bg={active ? 'glassTeal' : undefined}
         color={active ? '#202023' : inactiveColor}
+        target={target}
+        {...props}
       >
         {children}
       </Link>
@@ -52,7 +55,7 @@ const Navbar = props => {
         maxW='container.md'
         wrap='wrap'
         align='center'
-        justify='scape-between'
+        justify='space-between'
       >
         <Flex align='center' mr={5}>
           <Heading as='h1' size='lg' letterSpacing={'tighter'}>
@@ -66,7 +69,7 @@ const Navbar = props => {
           width={{ base: 'full', md: 'auto' }}
           alignItems='center'
           flexGrow={1}
-          mt={{ base: 4, nmd: 0 }}
+          mt={{ base: 4, md: 0 }}
         >
           <LinkItem href='/works' path={path}>
             Works
@@ -74,12 +77,25 @@ const Navbar = props => {
           <LinkItem href='/posts' path={path}>
             Posts
           </LinkItem>
+          <LinkItem
+            target='_blank'
+            href='https://github.com/jswent/jwswent-homepage'
+            path={path}
+            display='inline-flex'
+            alignItems='center'
+            style={{ gap: 4 }}
+            pl={2}
+          >
+            <IoLogoGithub />
+            Source
+          </LinkItem>
         </Stack>
 
         <Box flex={1} align='right'>
           <ThemeToggleButton />
-          <Box ml={2} display={{ base: 'inline-block' }}>
-            <Menu>
+
+          <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
+            <Menu isLazy id='navbar-menu'>
               <MenuButton
                 as={IconButton}
                 icon={<HamburgerIcon />}
@@ -96,8 +112,11 @@ const Navbar = props => {
                 <NextLink href='/posts' passHref>
                   <MenuItem as={Link}>Posts</MenuItem>
                 </NextLink>
-                <MenuItem as={Link} href='https://github.com/jswent'>
-                  View Source
+                <MenuItem
+                  as={Link}
+                  href='https://github.com/jswent/jwswent-homepage'
+                >
+                  <IoLogoGithub /> <Box p='1' opacity='0' /> View Source
                 </MenuItem>
               </MenuList>
             </Menu>
