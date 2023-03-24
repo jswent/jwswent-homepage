a8c18cf54399657599256fa56c697cbf6bf58be5
jswent <94636565+jswent@users.noreply.github.com>
2023-03-24 02:03:40 -0400

    vercel fix pt3

--- pages/api/requestVerification.js
+++ pages/api/requestVerification.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
   var otp = ''
   var length = string.length
 
-  await storage.init({ logging: true, ttl: 600000 })
+  await storage.init({ dir: '/tmp/node-persist/', logging: true, ttl: 600000 })
   let data = await storage.getItem(email)
 
   if (data) {
@@ -32,7 +32,7 @@ export default async function handler(req, res) {
 }
 
 async function getOtp(email) {
-  await storage.init({ logging: true, ttl: 600000 })
+  await storage.init({ dir:'/tmp/node-persist', logging: true, ttl: 600000 })
   let storageOtp = await storage.getItem(email)
   console.log(storageOtp)
 }
