6cfb97121e2d8aa6e061cabc81ebd9c3cbaa9aa1
James Swent <jake@osprey21.com>
2023-11-01 14:23:16 -0400

    update experience section

--- components/experience.js
+++ components/experience.js
@@ -3,6 +3,7 @@ import {
   VerticalTimeline,
   VerticalTimelineElement
 } from 'react-vertical-timeline-component'
+import Image from 'next/image'
 
 import { useColorModeValue } from '@chakra-ui/react'
 
@@ -11,7 +12,7 @@ import transocean from '../public/images/logos/transocean.png'
 import sta from '../public/images/logos/sta.png'
 import stech from '../public/images/logos/s-logo.png'
 import learnsight from '../public/images/logos/learnsight.png'
-import Image from 'next/image'
+import sandbox from '../public/images/logos/mit-sbif.jpeg'
 
 const experiences = [
   {
@@ -76,6 +77,27 @@ const experiences = [
       'Conducted primary market research to identify unmet needs in the Educational Technology sector and deliver a new-age collaborative learning platform',
       'Designed a product plan and mock-up and oversaw development to deliver an MVP to early adopters',
       'Hired and coordinated a team of developers across multiple time zones and countries'
+    ]
+  },
+  {
+    title: 'Manager of Systems & Development',
+    company_name: 'MIT Sandbox Innovation Fund Program',
+    icon: sandbox,
+    iconBg: '#fff',
+    date: 'March 2023 - present',
+    points: [
+      'Responsible for managing and maintaining existing infrastructure with the goal of developing an in-house end-to-end platform for startup applications, review, management, billing, and more...',
+      'Developed multiple internal applications for the team and external sites for events, fundraising, reporting, and more...',
+      'Released a new project management and documentation system and delivered training'
+    ]
+  },
+  {
+    title: 'Founder',
+    company_name: 'Digitall Uplift Management, LLC.',
+    iconBg: '#fff',
+    date: 'June 2023 - present',
+    points: [
+      'DigitALL Uplift is an accelerator for social ventures, where instead of providing dilutive funding we provide access to expert technical resources and advising through a network of developers and mentors wishing to volunteer their time',
     ]
   }
 ]
